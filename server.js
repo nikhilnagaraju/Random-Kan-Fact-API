6118b17f80ab4dc1a1dd6b51db1fd1159c553ec9
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2018-01-05 18:58:58 +0530

    added morgan

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
+const morgan         = require('morgan');
 
 const app            = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(morgan('dev'));
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
