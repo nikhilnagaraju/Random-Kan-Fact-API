32f7cfa785d79f121a98ec20ee57f1e15f52bcdd
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2018-01-05 19:00:57 +0530

    basic setup to return a random fact from db and also for random fact array

--- app/routes/fact_routes.js
+++ app/routes/fact_routes.js
@@ -3,10 +3,18 @@ module.exports = function(app, databaseObj) {
   const db = databaseObj.db('factsdb')
   var ObjectID = require('mongodb').ObjectID;
 
+  const mini=1;
+  var maxi=0;
+
 //GET random fact
-  app.get('/random/:id', (req, res) => {
-      // const id = req.params.id;
-      const details = { '_id': req.params.id };
+
+  app.get('/random', (req, res) => {
+      var dbcount =0;
+      // let coll = db.collection('factslist');
+      // var dbcount = coll.count().then(function() {
+      var getid = ""+getRandomIntInclusive(1, 260);
+
+      const details = { '_id': getid };
       db.collection('factslist').findOne(details, (err, item) => {
         if (err) {
           res.send({'error':'An error has occurred while fetching data'});
@@ -15,5 +23,38 @@ module.exports = function(app, databaseObj) {
         }
       });
   });
+
+  app.get('/facts', (req, res) => {
+      var idArr = getRandomArray();
+      var factitems= [];
+      var objt= {}
+      for (var i = 0; i < idArr.length; i++) {
+        var details = { '_id': idArr[i] };
+        db.collection('factslist').findOne(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred while fetching data'});
+          } else {
+            factitems.push(item);
+          }
+        });
+      }
+
+      res.send(factitems);
+  });
+
+  function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
+
+  function getRandomArray(){
+    randArr = [];
+    for (var i=0; i<10 ; i++){
+      var zz= "" + getRandomIntInclusive(1, 260);
+      randArr.push(zz);
+    }
+    return randArr;
+  }
 
 };
