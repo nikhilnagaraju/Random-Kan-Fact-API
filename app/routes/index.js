00b54d3e580a1f4f605753f0170378b1ea2689d4
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2020-05-02 16:00:24 +0530

    eslint refactoring

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,5 @@
 const appRoutes = require('./fact_routes');
 
-module.exports = function(app, db) {
+module.exports = function(app, db){
   appRoutes(app, db);
-  // Other route groups could go here, in the future
 };
