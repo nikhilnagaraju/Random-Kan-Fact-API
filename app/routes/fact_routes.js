e0f01c2fe97fe283ed9a3622d56be658f60fe1d3
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2020-05-02 16:00:24 +0530

    eslint refactoring

--- app/routes/fact_routes.js
+++ app/routes/fact_routes.js
@@ -1,57 +1,42 @@
-module.exports = function(app, databaseObj) {
+module.exports = function(app, databaseObj){
+  const db = databaseObj.db('factsdb');
 
-  const db = databaseObj.db('factsdb')
-  var ObjectID = require('mongodb').ObjectID;
-  var util = require('util');
-  // var stringify1 = require('json-stringify-safe');
-
-  const mini=1;
-  var maxi=0;
-
-
-  //GET Status for default path
+  // GET Status for default path
   app.get('/', (req, res) => {
-  var item = {
-    "status":"alive",
-    "message":"say thanks to SOF, API is alive",
-    "possible requests": {
-      "GET /random": "Get a random kannada fact",
-      "GET /facts": "Get an array of random kannada facts [array of size 10]",
-      "POST /fact": "Create/Add a fact on hosted db; requires a json with 'fact' model as body",
-      "PUT /fact/:id": "Update a fact using (integer)id on hosted DB; requires a json with 'fact' model as body",
-      "DELETE /fact/:id": "Delete a fact with (integer)id on hosted DB"
-    }};
+  const item = {
+    status: 'alive',
+    message: 'say thanks to SOF, API is alive',
+    'possible requests': {
+      'GET /random': 'Get a random kannada fact',
+      'GET /facts': 'Get an array of random kannada facts [array of size 10]',
+      'POST /fact': 'Create/Add a fact on hosted db; requires a json with \'fact\' model as body',
+      'PUT /fact/:id': 'Update a fact using (integer)id on hosted DB; requires a json with \'fact\' model as body',
+      'DELETE /fact/:id': 'Delete a fact with (integer)id on hosted DB'
+    }
+};
     res.send(item);
   });
 
-
-  //GET random fact
+  // GET random fact
   app.get('/random', (req, res) => {
-      var dbcount =0;
-      // let coll = db.collection('factslist');
-      // var dbcount = coll.count().then(function() {
-      var getid = ""+getRandomIntInclusive(1, 260);
-
-      const details = { '_id': getid };
+      const getid = '' + getRandomIntInclusive(1, 260);
+      const details = { _id: getid };
       db.collection('factslist').findOne(details, (err, item) => {
-        if (err) {
-          res.send({ "status" : "failure", 'error':'An error has occurred while fetching data' });
-        } else {
-          if (item)
-            res.send(item);
-          else
-            res.send({ "status" : "undetermined", 'error':'An error has occurred while fetching data' });
+        if(err){
+          res.send({ status: 'failure', error: 'An error has occurred while fetching data' });
+        }else{
+          if(item){ res.send(item); }else{ res.send({ status: 'undetermined', error: 'An error has occurred while fetching data' }); }
         }
       });
   });
 
   // GET an array of 10 Random Facts
   app.get('/facts', (req, res) => {
-      var objt= {};
-        db.collection('factslist').aggregate([{$match: {}}, { $sample: { size: 10 } }]).toArray( function (err, item) {
-          if (err) {
-            res.send({'error':'An error has occurred while fetching data'});
-          } else {
+      const objt = {};
+        db.collection('factslist').aggregate([{ $match: {} }, { $sample: { size: 10 } }]).toArray((err, item) => {
+          if(err){
+            res.send({ error: 'An error has occurred while fetching data' });
+          }else{
             objt.randomArray = item;
             console.log(objt);
             res.send(objt);
@@ -62,30 +47,29 @@ module.exports = function(app, databaseObj) {
   // POST a random fact
   app.post('/fact', (req, res) => {
     const fact = {
-                  _id: req.body.id ,
+                  _id: req.body.id,
                   enfact: req.body.enfact,
-                  knfact: req.body.knfact ,
+                  knfact: req.body.knfact,
                   imgurl: req.body.imgurl
                  };
-    if (fact._id == null || !typeof parseInt(fact.id) === 'number' || fact.knfact== null || fact.knfact== "" || fact.enfact== null || fact.enfact== ""){
+    if(fact._id == null || !typeof parseInt(fact.id) === 'number' || fact.knfact == null || fact.knfact === '' || fact.enfact == null || fact.enfact === ''){
       res.send({
-        "status" : "failure",
-        "error": "An error has occurred while posting data, Please request body should have valid data",
-        "valid data" : {
-          "id" : "an integer",
-          "enfact" : "string",
-          "knfact" : "string",
-          "imgurl" : "string"
+        status: 'failure',
+        error: 'An error has occurred while posting data, Please request body should have valid data',
+        'valid data': {
+          id: 'an integer',
+          enfact: 'string',
+          knfact: 'string',
+          imgurl: 'string'
         }
       });
-    }
-    else {
+    }else{
       db.collection('factslist').insert(fact, (err, result) => {
-        if (err) {
-          res.send({ "status" : "failure", "error": "An error has occurred while posting data, possibly a redundant data or an authorization error" });
+        if(err){
+          res.send({ status: 'failure', error: 'An error has occurred while posting data, possibly a redundant data or an authorization error' });
           console.log(err);
-        } else {
-          res.send({ "status" : "success", "fact added" : result.ops[0] });
+        }else{
+          res.send({ status: 'success', 'fact added': result.ops[0] });
         }
       });
     }
@@ -95,10 +79,10 @@ module.exports = function(app, databaseObj) {
   app.delete('/fact/:id', (req, res) => {
     const details = { _id: req.params.id };
     db.collection('factslist').remove(details, (err, item) => {
-      if (err) {
-        res.send({ "status" : "failure", "error":"An error has occurred while deleting data" });
-      } else {
-        res.send({ "status" : "success", "fact": req.params.id + " deleted" });
+      if(err){
+        res.send({ status: 'failure', error: 'An error has occurred while deleting data' });
+      }else{
+        res.send({ status: 'success', fact: req.params.id + ' deleted' });
       }
     });
   });
@@ -106,37 +90,27 @@ module.exports = function(app, databaseObj) {
   // UPDATE a Random fact with id
   app.put('/fact/:id', (req, res) => {
     const replace = { _id: req.params.id };
-    const fact = { enfact: req.body.enfact,
-                   knfact: req.body.knfact ,
+    const fact = {
+ enfact: req.body.enfact,
+                   knfact: req.body.knfact,
                    imgurl: req.body.imgurl
                  };
-    if ( !typeof parseInt(req.params.id) === 'number' || fact.knfact== null || fact.knfact== "" || fact.enfact== null || fact.enfact== ""){
-      res.send({ "status" : "failure", "error": "An error has occurred while posting data, Please provide valid data" });
-    }
-    else {
+    if(!typeof parseInt(req.params.id) === 'number' || fact.knfact == null || fact.knfact === '' || fact.enfact == null || fact.enfact === ''){
+      res.send({ status: 'failure', error: 'An error has occurred while posting data, Please provide valid data' });
+    }else{
       db.collection('factslist').update(replace, fact, (err, result) => {
-        if (err) {
-            res.send({ "status" : "failure", "error":"An error has occurred while updating data" });
-        } else {
+        if(err){
+            res.send({ status: 'failure', error: 'An error has occurred while updating data' });
+        }else{
             res.send(fact);
         }
       });
     }
   });
 
-  function getRandomIntInclusive(min, max) {
+  function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
-  function getRandomArray(){
-    randArr = [];
-    for (var i=0; i<10 ; i++){
-      var zz= "" + getRandomIntInclusive(1, 260);
-      randArr.push(zz);
-    }
-    return randArr;
-  }
-
 };
