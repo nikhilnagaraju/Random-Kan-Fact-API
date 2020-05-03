fd66dd7681fcf9480ba7ec9308349e04e4acf196
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2020-05-03 23:18:54 +0530

    prettier code style format

--- routes/fact_routes.js
+++ routes/fact_routes.js
@@ -1,144 +1,157 @@
 /* eslint-disable extra-rules/no-commented-out-code */
-const router = require('express').Router();
-const { normalizeCount, getRandomFacts, getFactByID, createFact, removeFactByID, updateFactByID } = require('./utils');
+const router = require("express").Router();
+const {
+  normalizeCount,
+  getRandomFacts,
+  getFactByID,
+  createFact,
+  removeFactByID,
+  updateFactByID,
+} = require("./utils");
 
-router.get('/', (_req, res) => {
+// let router.get(aa) = 2;
+router.get("/", (_req, res) => {
   const data = {
-    status: 'ok',
-    message: 'Why say Hello world!, when you can say HELLO UNIVERSE!',
+    status: "ok",
+    message: "Why say Hello world!, when you can say HELLO UNIVERSE!",
     endpoints: {
-      'GET /random': 'Get a random kannada fact',
-      'GET /facts': 'Get an array of random kannada facts [array of size 10]',
-      'POST /fact': 'Create/Add a fact on hosted db; requires a json with \'fact\' model as body',
-      'PUT /fact/:id': 'Update a fact using (integer)id on hosted DB; requires a json with \'fact\' model as body',
-      'DELETE /fact/:id': 'Delete a fact with (integer)id on hosted DB'
-    }
+      "GET /random": "Get a random kannada fact",
+      "GET /facts": "Get an array of random kannada facts [array of size 10]",
+      "POST /fact":
+        "Create/Add a fact on hosted db; requires a json with 'fact' model as body",
+      "PUT /fact/:id":
+        "Update a fact using (integer)id on hosted DB; requires a json with 'fact' model as body",
+      "DELETE /fact/:id": "Delete a fact with (integer)id on hosted DB",
+    },
   };
   res.json(data);
 });
 
 // GET random fact
-router.get('/random', async (req, res) => {
-  try{
+router.get("/random", async (req, res) => {
+  try {
     const factsArray = await getRandomFacts(req.app.db, 1);
-    if(factsArray.length > 0){
+    if (factsArray.length > 0) {
       res.send(factsArray[0]);
-    }else{
+    } else {
       res.status(400).json({
-        status: 'error',
-        message: 'No Data found.'
+        status: "error",
+        message: "No Data found.",
       });
     }
-  }catch(err){
+  } catch (err) {
     console.error(err);
-    res.status(400).json({ status: 'failure', error: 'An error has occurred while fetching data' });
+    res.status(400).json({
+      status: "failure",
+      error: "An error has occurred while fetching data",
+    });
   }
 });
 
 // GET an array of 10 Random Facts
-router.get('/facts', async (req, res) => {
+router.get("/facts", async (req, res) => {
   const size = normalizeCount(parseInt(req.query.count));
   const factsArray = await getRandomFacts(req.app.db, size);
-  if(factsArray.length !== size){
+  if (factsArray.length !== size) {
     res.status(400).json({
-      status: 'error',
-      message: 'No Data found.'
+      status: "error",
+      message: "No Data found.",
     });
-  }else{
+  } else {
     res.json({ facts: factsArray, count: factsArray.length });
   }
 });
 
-router.get('/facts/:id', async (req, res) => {
-  if(!req.params.id){
+router.get("/facts/:id", async (req, res) => {
+  if (!req.params.id) {
     res.status(400).json({
-      status: 'error',
-      message: 'Invalid Document ID'
+      status: "error",
+      message: "Invalid Document ID",
     });
-  };
-  try{
+  }
+  try {
     const fact = await getFactByID(req.app.db, req.params.id);
-    if(!fact){
+    if (!fact) {
       return res.status(404).json({
-        status: 'error',
-        message: `Document with ID ${req.params.id} doesn't exist.`
+        status: "error",
+        message: `Document with ID ${req.params.id} doesn't exist.`,
       });
     }
     res.json(fact);
-  }catch(error){
+  } catch (error) {
     res.status(400).json({
-      status: 'error',
-      message: 'Invalid document ID.'
+      status: "error",
+      message: "Invalid document ID.",
     });
   }
   return {};
 });
 
 // POST a random fact
-router.post('/facts', async (req, res) => {
-  if(!req.body.fact_en || !req.body.fact_kn){
+router.post("/facts", async (req, res) => {
+  if (!req.body.fact_en || !req.body.fact_kn) {
     res.status(400).json({
-      status: 'error',
-      message: 'Both \'fact_en\' and `fact_kn` data is required'
+      status: "error",
+      message: "Both 'fact_en' and `fact_kn` data is required",
     });
     return;
   }
   const data = {
     fact_en: req.body.fact_en,
     fact_kn: req.body.fact_kn,
-    imgurl: req.body.imgurl
+    imgurl: req.body.imgurl,
   };
-  try{
+  try {
     const result = await createFact(res.app.db, data);
     res.json(result.ops[0]);
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
-      status: 'error',
-      message: 'Failed to create fact'
+      status: "error",
+      message: "Failed to create fact",
     });
   }
 });
 
 // DELETE a Random fact with id
-router.delete('/facts/:id', async (req, res) => {
-  if(!req.params.id){
+router.delete("/facts/:id", async (req, res) => {
+  if (!req.params.id) {
     res.status(400).json({
-      status: 'error',
-      message: 'Invalid Document ID'
+      status: "error",
+      message: "Invalid Document ID",
     });
-  };
-  try{
+  }
+  try {
     await removeFactByID(req.app.db, req.params.id);
     res.json({ removedId: req.params.id });
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
-      status: 'error',
-      message: `Failed to delete Document with id ${req.params.id}`
+      status: "error",
+      message: `Failed to delete Document with id ${req.params.id}`,
     });
   }
 });
 
 // UPDATE a Random fact with id
-router.put('/facts/:id', async (req, res) => {
-  if(!req.body.fact_en || !req.body.fact_kn){
+router.put("/facts/:id", async (req, res) => {
+  if (!req.body.fact_en || !req.body.fact_kn) {
     res.status(400).json({
-      status: 'error',
-      message: 'Both \'fact_en\' and `fact_kn` data is required'
+      status: "error",
+      message: "Both 'fact_en' and `fact_kn` data is required",
     });
     return;
   }
   const fact = {
     fact_en: req.body.fact_en,
     fact_kn: req.body.fact_kn,
-    imgurl: req.body.imgurl
+    imgurl: req.body.imgurl,
   };
-  try{
+  try {
     const result = await updateFactByID(req.app.db, req.params.id, fact);
     res.json(result.value);
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
-      status: 'error',
-      message: 'Failed to update fact'
+      status: "error",
+      message: "Failed to update fact",
     });
   }
 });
--- routes/utils.js
+++ routes/utils.js
@@ -1,15 +1,18 @@
-const ObjectID = require('mongodb').ObjectID;
-const FACTS_COLLECTION = 'factslist';
+const ObjectID = require("mongodb").ObjectID;
+const FACTS_COLLECTION = "factslist";
 
 const normalizeCount = (size) => {
-  if(!size) size = 10;
-  if(size > 20) size = 20;
+  if (!size) size = 10;
+  if (size > 20) size = 20;
   return size;
 };
 
 const getRandomFacts = (db, size) => {
   const querySize = normalizeCount(size);
-  return db.collection(FACTS_COLLECTION).aggregate([{ $sample: { size: querySize } }]).toArray();
+  return db
+    .collection(FACTS_COLLECTION)
+    .aggregate([{ $sample: { size: querySize } }])
+    .toArray();
 };
 
 const getFactByID = (db, id) => {
@@ -25,7 +28,13 @@ const removeFactByID = (db, id) => {
 };
 
 const updateFactByID = (db, id, data) => {
-  return db.collection(FACTS_COLLECTION).findOneAndUpdate({ _id: ObjectID(id) }, { $set: data }, { returnOriginal: false });
+  return db
+    .collection(FACTS_COLLECTION)
+    .findOneAndUpdate(
+      { _id: ObjectID(id) },
+      { $set: data },
+      { returnOriginal: false }
+    );
 };
 
 module.exports = {
@@ -34,5 +43,5 @@ module.exports = {
   getFactByID,
   createFact,
   removeFactByID,
-  updateFactByID
+  updateFactByID,
 };
