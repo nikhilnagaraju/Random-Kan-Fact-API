065a2586fd5323f46054fdb8e684057be9212de1
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2020-05-03 23:43:30 +0530

    configuration with NODE env, bin files

--- config/db.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable extra-rules/no-commented-out-code */
-module.exports = {
-  // url : "mongodb://mclient:mclient@ds135917.mlab.com:35917/factsdb",
-  url: "mongodb://127.0.0.1:27017/factsdb",
-  // url : "mongodb://mclient:mclient@ds135917.mlab.com:35917/factsdb"
-};
